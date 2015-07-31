dcf4b20e8da218b4237aaeeaa5ae145ddc641398
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-31 14:42:11 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.spootnik/recordbus "0.1.0"
+(defproject org.spootnik/recordbus "0.1.1"
   :description "MySQL binlog to kafka"
   :url "https://github.com/pyr/recordbus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
   :main org.spootnik.recordbus
-  :dependencies [[org.clojure/clojure                           "1.6.0"]
+  :dependencies [[org.clojure/clojure                           "1.7.0"]
                  [org.clojure/tools.logging                     "0.3.1"]
-                 [org.spootnik/logconfig                        "0.7.3"]
-                 [cheshire                                      "5.4.0"]
-                 [com.github.shyiko/mysql-binlog-connector-java "0.1.2"]
+                 [spootnik/unilog                               "0.7.8"]
+                 [cheshire                                      "5.5.0"]
+                 [com.github.shyiko/mysql-binlog-connector-java "0.2.0"]
                  [org.apache.kafka/kafka-clients                "0.8.2.1"]])
